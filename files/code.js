451ec841f6324a4e0a49fdca8ffb29f0a88835bf
develeone <admin@awake.su>
2017-04-19 17:53:29 +1000

    Сделал нормальный вывод в консоль данных

--- files/code.js
+++ files/code.js
@@ -7,31 +7,26 @@ for (var i = 0; i < ROWS_COUNT; i++) {
 
 	rows_sockets[i].row_id = i; // Устанавливаем в параметр - id стойки, чтоб в асинхронных функциях использовать
 
-	rows_sockets[i].onopen = function() { console.log("Соединение установлено."); };
-
+	rows_sockets[i].onopen = function() { console.log("Соединение стойки "+this.row_id+" установлено."); };
 	rows_sockets[i].onclose = function(event) {
-	    if (event.wasClean)
-		    alert('Соединение закрыто корректно');
-	    else
-		    alert('Обрыв соединения');
-
-	    alert('Код: ' + event.code + ' причина: ' + event.reason);
+        alert((event.wasClean ? 'Соединение стойки '+this.row_id+' закрыто корректно'
+                : 'Обрыв соединения стойки '+this.row_id)+'\n'+
+                'Код: ' + event.code + ' причина: ' + event.reason );
 	};
-
-	rows_sockets[i].onmessage = function(event) {
-	    console.log(event.data);
-	    processMessage(event.data, this.row_id);
-	};
-
-	rows_sockets[i].onerror = function(error) {
-	    alert("Ошибка " + error.message);
-	};
+	rows_sockets[i].onmessage = function(event) { processMessage(event.data, this.row_id); };
+	rows_sockets[i].onerror = function(error) { alert("Ошибка стойки " + this.row_id +": "+ error.message); };
 }
 
 function processMessage (data, row_id) {
 	data = JSON.parse(data);
 
 	if (data.action == 1)
-        for(var i = 0; i < data.numbers_arr.length; i++)
-            document.getElementById("cell-"+row_id+"-"+i).innerHTML = data.numbers_arr[i];
+	    if (data.numbers_arr.length == 24) {
+            for (var i = 0; i < data.numbers_arr.length; i++)
+                document.getElementById("cell-" + row_id + "-" + i).innerHTML = data.numbers_arr[i];
+            console.log("На стойку "+row_id+" установлены данные " + data.numbers_arr);
+        } else
+	        console.error("Для стойки "+row_id+" получено неверное количество значений: "+data.numbers_arr.length);
+    else
+        console.log(event.data);
 }
